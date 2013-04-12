d8644c864aafce2414ca8ed0a7126e9f7c0c3c48
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-12 12:36:10 +0100

    Add DB schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE company (
+id integer PRIMARY_KEY,
+name text
+);
+CREATE TABLE search (
+id integer PRIMARY_KEY,
+searchcompany integer,
+searchoption integer,
+field1 text,
+field2 text,
+field3 text,
+field4 text,
+field5 text
+);
