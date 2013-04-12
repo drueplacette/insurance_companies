673e90929f8f4515bc468714f75a35db36cb2146
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-12 19:33:18 +0100

    Cleaned up parse_records, new helper uses list comprehensions instead of filters and maps.

--- scripts/build_db.py
+++ scripts/build_db.py
@@ -21,17 +21,25 @@ def csv_parselines(filepath):
 
 def parse_records(row):
     '''Takes a flat list and returns a tuple containing the company name and a list of numbered options'''
-    company = row[0]
-    search_options = [row[2:6], row[8:13]] + [row[(i*6)+14+1:(i*6)+14+6] for i in range(9)] # fallback approach due to uneven field sizes. Messy.
-    search_options = map(lambda option: filter(lambda field: field != '', option), search_options) # Remove empty fields
-    search_options = filter(lambda option: option[1] != [], list(enumerate(search_options, start=1))) # Remove empty search options, number correctly
-    search_options = map(lambda option: [option[0]] + option[1], search_options) # Flatten options list to include option no.
-
-    return {'company':company, 'search_options':search_options}
+    return {'company':row[0], 'search_options':_parse_search_options(row)}
 
 def db_insert_records(parsed_records):
     '''Inserts parsed records into the database'''
     pass
+
+def _parse_search_options(row):
+    # fallback approach due to uneven field sizes. Messy.
+    search_options = [row[2:6], row[8:13]] + [row[(i*6)+14+1:(i*6)+14+6] for i in range(9)]
+    # Remove empty fields from options
+    search_options = [[field for field in option if field != ''] 
+                        for option in search_options]
+    # Number options and remove empty options
+    search_options = [option for option in enumerate(search_options, start=1)
+                        if ''.join(option[1]) != '']
+    # Flatten option number into list
+    search_options = [[option[0]] + option[1] for option in search_options]
+
+    return search_options
 
 if __name__ == '__main__':
     main(csv_in=argv[1], sqlite_out=argv[2]) # Placeholder until proper argparse put in place
