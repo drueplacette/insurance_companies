aaf6f6b0f124c12a0f46f79fff86ad68dc78ffa6
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-12 12:46:09 +0100

    Add template for csv-to-sqlite conversion script.

---
+++ scripts/build_db.py
@@ -0,0 +1,16 @@
+import csv, sqlite3
+
+def main(csv_in, sqlite_out):
+    pass
+
+def csv_readlines(filepath):
+    pass
+
+def parse_records(searchinfo):
+    pass
+
+def db_insert_records(parsed_records):
+    pass
+
+if __name__ == '__main__':
+    main(csv_in=sys.argv[1], sqlite_out=sys.argv[2])
