d962f9bcdd43df950dbb0e5c81eece68dd9dcf03
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-18 15:38:52 +0100

    Add database config

---
+++ config/database.py
@@ -0,0 +1,4 @@
+'''Configuration settings for the database'''
+
+# See http://docs.sqlalchemy.org/en/rel_0_8/core/engines.html for more information
+database_URI = 'sqlite:///db/search_database.sqlite'
