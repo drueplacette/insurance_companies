c8cae154f9615826eccc9317ba516465f3ec7e99
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-19 17:12:39 +0100

    Add working project imports to the population script

--- bin/populate_database.py
+++ bin/populate_database.py
@@ -2,21 +2,33 @@
 Takes an excel-generated CSV file of insurance companies 
 and possible search queries and writes it to the app Database
 '''
-import argparse, os
+import argparse, os, sys, csv
+
+# Path magic, need the models and configs from elsewhere
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..')))
+from models import Base, Company, SearchOption
+from config.database import database_URI
 
 parser = argparse.ArgumentParser(prog='populate_database')
 parser.add_argument('csv_source', help='the excel-exported csv for building the database')
-parser.add_argument('-d', help='URI of the database', default=os.environ.get("DATABASE_URI"))
 args = parser.parse_args()
 
-def main():
+def main(csv_filepath):
     '''Iterate over lines of a CSV, parsing records and insterting them into a database'''
-    pass
+    for row, record in enumerate(csv_parselines(csv_filepath)):
+        pass # Insert into database
 
-def csv_parselines(csvfile):
+def csv_parselines(csv_filepath):
     '''Yield parsed CSV lines one at a time'''
-    pass
+    with open(csv_filepath, 'r') as csv_file:
+       reader = csv.reader(csv_file, dialect='excel')
+       next(reader) # Skip headings
+       for record in reader:
+           yield parse_line(record)
 
-def parse_line(line):
+def parse_record(record):
     '''Parse a single line from a CSV'''
-    pass
+    return {'company':row[0], 'payer_id':row[1].upper(), 'search_options':_parse_search_options(row)}
+
+if __name__ == '__main__':
+    main(args.csv_source)
